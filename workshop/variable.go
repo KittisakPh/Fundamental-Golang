d8c6db1202c4620e734f1fad82ffcd77940dafd3
Kittisak2001 <kittisak.ph2001@gmail.com>
2025-01-27 14:50:02 +0700

    fix(workshop): implement in code

---
+++ workshop/variable.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Workshop: Println
+	// Output:
+	// เรื่อง: Avengers: Endgame
+	// ปี: 2019
+	// เรตติ้ง: 8.4
+	// ประเภท: Sci-Fi
+	// ซุปเปอร์ฮีโร่: true
+	// TODO: write code below.
+	fmt.Println("เรื่อง: Avengers: Endgame")
+	fmt.Println("ปี: 2019")
+	fmt.Println("เรตติ้ง: 8.4")
+	fmt.Println("ประเภท: Sci-Fi")
+	fmt.Println("ซุปเปอร์ฮีโร่: true")
+}
