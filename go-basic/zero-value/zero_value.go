d3875f0966922fc7524a0a46da854486dcceed49
Kittisak2001 <kittisak.ph2001@gmail.com>
2025-01-28 00:46:23 +0700

    feat(zero-value): init

---
+++ go-basic/zero-value/zero_value.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var msg string
+	var age int
+	var price float64
+	var check bool
+	var r rune
+	
+	fmt.Printf("msg: %q\n", msg)
+	fmt.Printf("age: %d\n", age)
+	fmt.Printf("price: %f\n", price)
+	fmt.Printf("check: %t\n", check)
+	fmt.Printf("r: %d\n", r)
+}
