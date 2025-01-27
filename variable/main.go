163b3b6f101d7281f8840cd959e11f31025d55dc
Kittisak2001 <kittisak.ph2001@gmail.com>
2025-01-27 14:16:02 +0700

    feat(variable): init variable

---
+++ variable/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// var msg string = "Hello Gopher!!!"
+
+func main() {
+	var msg, age, price, check = "Hello Gopher!!!", 55, 22.52, true
+
+	fmt.Println("msg:", msg)
+	fmt.Println("age:", age)
+	fmt.Println("price:", price)
+	fmt.Println("check:", check)
+}
