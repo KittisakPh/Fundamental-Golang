f936d465d63e852c7290712051672b03c0069e17
Kittisak2001 <kittisak.ph2001@gmail.com>
2025-01-28 00:37:10 +0700

    feat(variable): init folder go-basic for variable

--- go-basic/variable/main.go
+++ go-basic/variable/main.go
@@ -5,7 +5,7 @@ import "fmt"
 // var msg string = "Hello Gopher!!!"
 
 func main() {
-	var msg, age, price, check = "Hello Gopher!!!", 55, 22.52, true
+	msg, age, price, check := "Hello Gopher!!!", 55, 22.52, true
 
 	fmt.Println("msg:", msg)
 	fmt.Println("age:", age)
