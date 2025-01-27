d39ccd19e05c783bb7621a2f22c29a9525156a74
Kittisak2001 <kittisak.ph2001@gmail.com>
2025-01-27 14:55:03 +0700

    fix(workshop): v2 of example

--- workshop/variable.go
+++ workshop/variable.go
@@ -2,18 +2,20 @@ package main
 
 import "fmt"
 
+// Workshop: Println
+// Output:
+// เรื่อง: Avengers: Endgame
+// ปี: 2019
+// เรตติ้ง: 8.4
+// ประเภท: Sci-Fi
+// ซุปเปอร์ฮีโร่: true
+// TODO: write code below.
+
 func main() {
-	// Workshop: Println
-	// Output:
-	// เรื่อง: Avengers: Endgame
-	// ปี: 2019
-	// เรตติ้ง: 8.4
-	// ประเภท: Sci-Fi
-	// ซุปเปอร์ฮีโร่: true
-	// TODO: write code below.
-	fmt.Println("เรื่อง: Avengers: Endgame")
-	fmt.Println("ปี: 2019")
-	fmt.Println("เรตติ้ง: 8.4")
-	fmt.Println("ประเภท: Sci-Fi")
-	fmt.Println("ซุปเปอร์ฮีโร่: true")
+	title, year, ratings, genre, isSuperHero := "Avengers: Endgame", "2019", "8.4", "Sci-Fi", true
+	fmt.Println("เรื่อง:", title)
+	fmt.Println("ปี:", year)
+	fmt.Println("เรตติ้ง:", ratings)
+	fmt.Println("ประเภท:", genre)
+	fmt.Println("ซุปเปอร์ฮีโร่:", isSuperHero)
 }
