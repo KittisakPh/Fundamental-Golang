87d9ed59819382dd2daa0d4a174dafdc9644038f
Kittisak2001 <kittisak.ph2001@gmail.com>
2025-01-27 14:58:38 +0700

    feat(workshop): technique Raw string in variable

--- workshop/variable.go
+++ workshop/variable.go
@@ -12,10 +12,19 @@ import "fmt"
 // TODO: write code below.
 
 func main() {
-	title, year, ratings, genre, isSuperHero := "Avengers: Endgame", "2019", "8.4", "Sci-Fi", true
+	// ->use variable
+	title, year, ratings, genre, isSuperHero := "Avengers: Endgame", 2019, 8.4, "Sci-Fi", true
 	fmt.Println("เรื่อง:", title)
 	fmt.Println("ปี:", year)
 	fmt.Println("เรตติ้ง:", ratings)
 	fmt.Println("ประเภท:", genre)
 	fmt.Println("ซุปเปอร์ฮีโร่:", isSuperHero)
+
+	// ->use Raw string
+	movie :=`เรื่อง: Avengers: Endgame
+ปี: 2019
+เรตติ้ง: 8.4
+ประเภท: Sci-Fi
+ซุปเปอร์ฮีโร่: true`
+	fmt.Println(movie)
 }
